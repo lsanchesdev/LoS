55772895e2554bd04947260bf92ab5a76521c678
Leandro Sanches <contato@leandrosanches.com.br>
2017-01-18 09:35:15 -0200

    Minor Changes

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # LoS
+
+Second atempt to build a BootLoader and a Kernel in C/C++.
+
+## BootLoader
+
+### [Jan. 18th] 
+* BootLoader running in 16-bits mode.
+* BootLoader is now able to print messages.
+
+## Build
+
+```
+nasm bootloader/bootloader.asm -f bin -o bootloader/bootloader.bin
+```
+
+```
+dd if=bootloader/bootloader.bin bs=512 of=bootloader/bootloader-vm.img
+```
---
+++ bootloader.asm
@@ -0,0 +1,53 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Vars and Constants
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+[BITS 16] ; 16-bits architecture
+[ORG 0x7C00] ; Where the code will be in memory after it's loaded
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Instructions and Loop
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+MOV SI, MainMessage ; Sends the string to SI
+CALL Echo ; Call print function
+JMP $ ; Infinite Loop
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Functions
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+PrintChar: ; Function Name
+	
+	; Assume that ASCII value is in register AL
+	MOV AH, 0x0E ; Tell BIOS that we need to print one charater on screen.
+	MOV BH, 0x00 ; Page number
+	MOV BL, 0x07 ; Text attribute 0x07 is lightgrey font on black background
+
+	INT 0x10 ; Call video interrupt
+	RET	; Return
+
+Echo: ; Function Name
+
+	; Assume that string starting pointer is in register SI
+	next_character:	; Function to fetch next character from string
+		MOV AL, [SI] ; Get a byte from string and store in AL register
+		INC SI ; Increment SI pointer
+		OR AL, AL ; Check if value in AL is zero (end of string)
+		JZ exit_function ; If end then return
+		CALL PrintChar ; Else print the character which is in AL register
+		JMP next_character	; Fetch next character from string
+	exit_function: ; End next_character
+	RET ; Return
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Data
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+MainMessage db 'Hello LoS 16-bits', 0 ; HelloWorld string ending with 0
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Filling the boot sector
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+TIMES 510 - ($ - $$) db 0 ; Fill the rest of sector with 0
+DW 0xAA55 ; Boot Signature at the end of the loader
